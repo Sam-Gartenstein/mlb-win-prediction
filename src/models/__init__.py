700415198f1e0c4da7889da02af1a541b9e75d46
Samuel Gartenstein <samgart98@aol.com>
2026-03-10 11:17:06 -0400

    Add Bayesian model import to src/models __init__

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,6 +3,7 @@ from .machine_learning.utils import (
     print_best_model_report,
     plot_model_feature_effects,
 )
+from .bayesian import fit_bayesian_logistic
 
 __all__ = [
     "fit_eval_logreg",
@@ -10,4 +11,5 @@ __all__ = [
     "fit_eval_xgb",
     "print_best_model_report",
     "plot_model_feature_effects",
+    "fit_bayesian_logistic",
 ]
